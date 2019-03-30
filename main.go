8882fcef1a601a3bc844885898bd7d1d41d71627
rihofujino <r-fujino@tribalmedia.co.jp>
2019-03-30 20:55:37 +0900

    edit main.go

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -10,6 +11,7 @@ import (
 )
 
 func main() {
+	fmt.Println("server start ...")
 	port := os.Getenv("PORT")
 
 	if port == "" {
