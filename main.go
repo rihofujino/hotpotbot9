11bb93be7e9ecca80155644858444c2de9f90a74
rihofujino <r-fujino@tribalmedia.co.jp>
2019-03-30 21:43:39 +0900

    edit main.go

--- main.go
+++ main.go
@@ -1,108 +1,71 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"fmt"
+	"math/rand"
+	"time"
+
 	"github.com/gin-gonic/gin"
-	// ① SDKを追加
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 func main() {
-	fmt.Println("server start ...")
 	port := os.Getenv("PORT")
-	fmt.Println(port)
 
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
 
 	router := gin.New()
-	// router.Use(gin.Logger())
-	// router.LoadHTMLGlob("templates/*.tmpl.html")
-	// router.Static("/static", "static")
+	router.Use(gin.Logger())
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	ChannelSecret := os.Getenv("CHANNEL_SECRET")
+	ChannelAccessToken := os.Getenv("CHANNEL_TOKEN")
+
+	router.POST("/hook", func(c *gin.Context) {
+		client := &http.Client{Timeout: time.Duration(15 * time.Second)}
+		bot, err := linebot.New(ChannelSecret, ChannelAccessToken, linebot.WithHTTPClient(client))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		received, err := bot.ParseRequest(c.Request)
+
+		for _, event := range received {
+			if event.Type == linebot.EventTypeMessage {
+				switch message := event.Message.(type) {
+				case *linebot.TextMessage:
+					source := event.Source
+					if source.Type == linebot.EventSourceTypeRoom {
+						if resMessage := getResMessage(message.Text); resMessage != "" {
+							postMessage := linebot.NewTextMessage(resMessage)
+							if _, err = bot.ReplyMessage(event.ReplyToken, postMessage).Do(); err != nil {
+								log.Print(err)
+							}
+						}
+					}
+				}
+			}
+		}
 	})
 
 	router.Run(":" + port)
-
-	// r := mux.NewRouter()
-	// r.HandleFunc("/", testpage)
-	// r.HandleFunc("/webhook", mainhandler).Methods("POST")
-
-	// mux := http.NewServeMux()
-	// fmt.Println(mux)
-	// mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Println("来た")
-	// 	fmt.Fprintf(w, "Welcome to the home page!")
-	// })
-
-	// mux.HandleFunc("/webhook", func(w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Println("来た")
-	// 	bot, err := linebot.New(
-	// 		os.Getenv("CHANNEL_SECRET"),
-	// 		os.Getenv("CHANNEL_TOKEN"),
-	// 	)
-	// 	if err != nil {
-	// 		log.Print(err)
-	// 	}
-	// 	events, err := bot.ParseRequest(r)
-	// 	if err != nil {
-	// 		if err == linebot.ErrInvalidSignature {
-	// 			log.Print(err)
-	// 		}
-	// 		return
-	// 	}
-	// 	for _, event := range events {
-	// 		if event.Type == linebot.EventTypeMessage {
-	// 			switch message := event.Message.(type) {
-	// 			case *linebot.TextMessage:
-	// 				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
-	// 					log.Print(err)
-	// 				}
-	// 			}
-	// 		}
-	// 	}
-	// })
-
-	// if err := http.ListenAndServe(":"+port, nil); err != nil {
-	// 	log.Print(err)
-	// }
 }
 
-// //MainPage ...
-// func testpage(w http.ResponseWriter, r *http.Request) {
-// 	w.Write([]byte("hi"))
-// }
+func getResMessage(reqMessage string) (message string) {
+	resMessages := [3]string{"わかるわかる", "それで？それで？", "からの〜？"}
 
-// func mainhandler(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Println("来た")
-// 	bot, err := linebot.New(
-// 		os.Getenv("CHANNEL_SECRET"),
-// 		os.Getenv("CHANNEL_TOKEN"),
-// 	)
-// 	if err != nil {
-// 		log.Print(err)
-// 	}
-// 	events, err := bot.ParseRequest(r)
-// 	if err != nil {
-// 		if err == linebot.ErrInvalidSignature {
-// 			log.Print(err)
-// 		}
-// 		return
-// 	}
-// 	for _, event := range events {
-// 		if event.Type == linebot.EventTypeMessage {
-// 			switch message := event.Message.(type) {
-// 			case *linebot.TextMessage:
-// 				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
-// 					log.Print(err)
-// 				}
-// 			}
-// 		}
-// 	}
-// }
+	rand.Seed(time.Now().UnixNano())
+	if rand.Intn(5) == 0 {
+		if math := rand.Intn(4); math != 3 {
+			message = resMessages[math]
+		} else {
+			message = reqMessage + "じゃねーよw"
+		}
+	}
+	return
+}
