e6b5c62524de90d445d6bdfa0d204d901504fff9
rihofujino <r-fujino@tribalmedia.co.jp>
2019-03-30 21:38:58 +0900

    edit main.go

--- main.go
+++ main.go
@@ -6,7 +6,8 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/line/line-bot-sdk-go/linebot" // ① SDKを追加
+	"github.com/gin-gonic/gin"
+	// ① SDKを追加
 )
 
 func main() {
@@ -18,48 +19,59 @@ func main() {
 		log.Fatal("$PORT must be set")
 	}
 
+	router := gin.New()
+	// router.Use(gin.Logger())
+	// router.LoadHTMLGlob("templates/*.tmpl.html")
+	// router.Static("/static", "static")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
+
+	router.Run(":" + port)
+
 	// r := mux.NewRouter()
 	// r.HandleFunc("/", testpage)
 	// r.HandleFunc("/webhook", mainhandler).Methods("POST")
 
-	mux := http.NewServeMux()
-	fmt.Println(mux)
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("来た")
-		fmt.Fprintf(w, "Welcome to the home page!")
-	})
+	// mux := http.NewServeMux()
+	// fmt.Println(mux)
+	// mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Println("来た")
+	// 	fmt.Fprintf(w, "Welcome to the home page!")
+	// })
 
-	mux.HandleFunc("/webhook", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("来た")
-		bot, err := linebot.New(
-			os.Getenv("CHANNEL_SECRET"),
-			os.Getenv("CHANNEL_TOKEN"),
-		)
-		if err != nil {
-			log.Print(err)
-		}
-		events, err := bot.ParseRequest(r)
-		if err != nil {
-			if err == linebot.ErrInvalidSignature {
-				log.Print(err)
-			}
-			return
-		}
-		for _, event := range events {
-			if event.Type == linebot.EventTypeMessage {
-				switch message := event.Message.(type) {
-				case *linebot.TextMessage:
-					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
-						log.Print(err)
-					}
-				}
-			}
-		}
-	})
+	// mux.HandleFunc("/webhook", func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Println("来た")
+	// 	bot, err := linebot.New(
+	// 		os.Getenv("CHANNEL_SECRET"),
+	// 		os.Getenv("CHANNEL_TOKEN"),
+	// 	)
+	// 	if err != nil {
+	// 		log.Print(err)
+	// 	}
+	// 	events, err := bot.ParseRequest(r)
+	// 	if err != nil {
+	// 		if err == linebot.ErrInvalidSignature {
+	// 			log.Print(err)
+	// 		}
+	// 		return
+	// 	}
+	// 	for _, event := range events {
+	// 		if event.Type == linebot.EventTypeMessage {
+	// 			switch message := event.Message.(type) {
+	// 			case *linebot.TextMessage:
+	// 				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+	// 					log.Print(err)
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// })
 
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Print(err)
-	}
+	// if err := http.ListenAndServe(":"+port, nil); err != nil {
+	// 	log.Print(err)
+	// }
 }
 
 // //MainPage ...
