df3e5ecccfadcf85da780db5a2aae950d8223706
rihofujino <r-fujino@tribalmedia.co.jp>
2019-03-30 20:29:52 +0900

    main.goを編集

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/gin-gonic/gin"
+	"github.com/gorilla/mux"
 	"github.com/line/line-bot-sdk-go/linebot" // ① SDKを追加
 )
 
@@ -17,6 +17,51 @@ func main() {
 	}
 
 	// ② LINE bot instanceの作成
+	// bot, err := linebot.New(
+	// 	os.Getenv("CHANNEL_SECRET"),
+	// 	os.Getenv("CHANNEL_TOKEN"),
+	// )
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	r := mux.NewRouter()
+	r.HandleFunc("/webhook", mainhandler)
+
+	// router := gin.New()
+	// router.Use(gin.Logger())
+	// router.LoadHTMLGlob("templates/*.tmpl.html")
+	// router.Static("/static", "static")
+
+	// router.GET("/", func(c *gin.Context) {
+	// 	c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	// })
+
+	// ③ LINE Messaging API用の Routing設定
+	// router.POST("/webhook", func(c *gin.Context) {
+	// 	events, err := bot.ParseRequest(c.Request)
+	// 	if err != nil {
+	// 		if err == linebot.ErrInvalidSignature {
+	// 			log.Print(err)
+	// 		}
+	// 		return
+	// 	}
+	// 	for _, event := range events {
+	// 		if event.Type == linebot.EventTypeMessage {
+	// 			switch message := event.Message.(type) {
+	// 			case *linebot.TextMessage:
+	// 				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+	// 					log.Print(err)
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// })
+
+	// router.Run(":" + port)
+}
+
+func mainhandler(w http.ResponseWriter, r *http.Request) {
 	bot, err := linebot.New(
 		os.Getenv("CHANNEL_SECRET"),
 		os.Getenv("CHANNEL_TOKEN"),
@@ -24,36 +69,21 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
-
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
-
-	// ③ LINE Messaging API用の Routing設定
-	router.POST("/webhook", func(c *gin.Context) {
-		events, err := bot.ParseRequest(c.Request)
-		if err != nil {
-			if err == linebot.ErrInvalidSignature {
-				log.Print(err)
-			}
-			return
+	events, err := bot.ParseRequest(r)
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			log.Print(err)
 		}
-		for _, event := range events {
-			if event.Type == linebot.EventTypeMessage {
-				switch message := event.Message.(type) {
-				case *linebot.TextMessage:
-					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
-						log.Print(err)
-					}
+		return
+	}
+	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+					log.Print(err)
 				}
 			}
 		}
-	})
-
-	router.Run(":" + port)
+	}
 }
