a2220e98d5a690aa37c3939419f107938ffd777b
rihofujino <r-fujino@tribalmedia.co.jp>
2019-04-05 16:26:12 +0900

    edit main.go

--- main.go
+++ main.go
@@ -39,7 +39,7 @@ func main() {
 			return
 		}
 		for _, event := range events {
-			if event.Type == linebot.EventTypeMessage {
+			if event.Type == linebot.EventTypeFollow {
 				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewFlexMessage("alt text", container)).Do(); err != nil {
 					log.Print(err)
 				}
