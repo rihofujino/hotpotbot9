b7f0a34a4bc62c7917619640f9e47ce45a41779f
rihofujino <r-fujino@tribalmedia.co.jp>
2019-04-05 15:20:49 +0900

    edit main.go

--- main.go
+++ main.go
@@ -5,72 +5,43 @@ import (
 	"net/http"
 	"os"
 
-	"fmt"
-	"math/rand"
-	"time"
-
-	"github.com/gin-gonic/gin"
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-
-	if port == "" {
-		log.Fatal("$PORT must be set")
+	bot, err := linebot.New(
+		os.Getenv("CHANNEL_SECRET"),
+		os.Getenv("CHANNEL_TOKEN"),
+	)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	router := gin.New()
-	router.Use(gin.Logger())
-
-	ChannelSecret := os.Getenv("CHANNEL_SECRET")
-	ChannelAccessToken := os.Getenv("CHANNEL_TOKEN")
-
-	router.POST("/webhook", func(c *gin.Context) {
-		client := &http.Client{Timeout: time.Duration(15 * time.Second)}
-
-		bot, err := linebot.New(ChannelSecret, ChannelAccessToken, linebot.WithHTTPClient(client))
+	// Setup HTTP Server for receiving requests from LINE platform
+	http.HandleFunc("/webhook", func(w http.ResponseWriter, req *http.Request) {
+		events, err := bot.ParseRequest(req)
 		if err != nil {
-			fmt.Println(err)
+			if err == linebot.ErrInvalidSignature {
+				w.WriteHeader(400)
+			} else {
+				w.WriteHeader(500)
+			}
 			return
 		}
-		fmt.Println(*bot) //&{6aa37b7773a27d966174457ccb5c284e ix44e08i7lia3MIrspbXvrrSHfTxlpPMgSAoyggevENTdxnatN0RSgowwsk/KZgtBSCpoB9osesTOo30NuzFxxgnIWCmUCtYrCU8AhARnMfx38MumaPD1RnfzvCKYyxhhLCaMF58vIlEJhzKYq7piQdB04t89/1O/w1cDnyilFU= 0xc0001f6300 0xc0001b6e40}
-		received, err := bot.ParseRequest(c.Request)
-
-		for _, event := range received {
-			fmt.Println(*event) //&{9f5422904b2d4b70ac3a7305ed334b42 message 2019-03-30 12:55:39.218 +0000 UTC 0xc0001bc600 0xc0001b7110 <nil> <nil> <nil> <nil> <nil> <nil> []}
+		for _, event := range events {
 			if event.Type == linebot.EventTypeMessage {
 				switch message := event.Message.(type) {
 				case *linebot.TextMessage:
-					source := event.Source
-					fmt.Println(source) //&{user U12d726bedeaf40594255d81c263c0f0f  }
-					if source.Type == linebot.EventSourceTypeUser {
-						if resMessage := getResMessage(message.Text); resMessage != "" {
-							postMessage := linebot.NewTextMessage(resMessage)
-							if _, err = bot.ReplyMessage(event.ReplyToken, postMessage).Do(); err != nil {
-								log.Print(err)
-							}
-						}
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+						log.Print(err)
 					}
 				}
 			}
 		}
 	})
-
-	router.Run(":" + port)
-}
-
-func getResMessage(reqMessage string) (message string) {
-	resMessages := [3]string{"わかるわかる", "それで？それで？", "からの〜？"}
-
-	rand.Seed(time.Now().UnixNano())
-	if rand.Intn(5) == 0 {
-		if math := rand.Intn(4); math != 3 {
-			message = resMessages[math]
-		} else {
-			message = reqMessage + "じゃねーよw"
-		}
-		fmt.Println(message)
+	// This is just sample code.
+	// For actual use, you must support HTTPS by using `ListenAndServeTLS`, a reverse proxy or something else.
+	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
+		log.Fatal(err)
 	}
-	return
 }
