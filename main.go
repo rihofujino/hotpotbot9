5f8250fe4acea814c18a31b05f310fc40d34ba35
rihofujino <r-fujino@tribalmedia.co.jp>
2019-04-20 15:29:46 +0900

    edit main.go

--- main.go
+++ main.go
@@ -6,26 +6,20 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/gorilla/mux"
 	"github.com/heroku/gyozabot/handlers"
 )
 
 func main() {
 	fmt.Println("Server Start...")
 
-	r := mux.NewRouter()
-	fmt.Println("debug1")
-	r.HandleFunc("/webhook", handlers.Main)
-	r.HandleFunc("/personal-info", handlers.PersonalInfo)
-
-	port := os.Getenv("PORT")
-	fmt.Println(port)
-
-	log.Fatal(http.ListenAndServe(port, r))
+	// r := mux.NewRouter()
+	// fmt.Println("debug1")
+	http.HandleFunc("/webhook", handlers.Main)
+	http.HandleFunc("/personal-info", handlers.PersonalInfo)
 
 	// This is just sample code.
 	// For actual use, you must support HTTPS by using `ListenAndServeTLS`, a reverse proxy or something else.
-	// if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
-	// 	log.Fatal(err)
-	// }
+	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
+		log.Fatal(err)
+	}
 }
